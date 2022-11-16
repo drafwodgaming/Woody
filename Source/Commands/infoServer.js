bf4426b2699785ba997e5f8c7677edfc9742fe5d
drafwod <drafwodgaming@yandex.ru>
2022-11-16 05:57:12 +0300

    /infoserver

--- Source/Commands/infoServer.js
+++ Source/Commands/infoServer.js
@@ -1,70 +1,99 @@
 const { SlashCommandBuilder, ChannelType, messageLink } = require("discord.js");
-const { embedSetup } = require("../functions/embedSetup");
+const { embedSetup } = require("../Functions/embedSetup");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("serverinfo")
     .setDescription("Информация о сервере"),
   async execute(interaction) {
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: ИНФОРМАЦИЯ О СЕРВЕРЕ
+     * ! --------------------------------
+     */
+    const guildName = interaction.guild.name;
+    const guildOwner = interaction.guild.ownerId;
+    const guildCreatedTimestamp = parseInt(
+      interaction.guild.createdTimestamp / 1000
+    );
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: ПОЛЬЗОВАТЕЛИ
+     * ! --------------------------------
+     */
+    const guildMemberCount = interaction.guild.memberCount;
+    const guildMembers = interaction.guild.members.cache.filter(
+      (m) => !m.user.bot
+    ).size;
+    const guildBots = interaction.guild.members.cache.filter(
+      (m) => m.user.bot
+    ).size;
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: КАНАЛЫ
+     * ! --------------------------------
+     */
+    const guildChannels = interaction.guild.channels.cache.size;
+    const guildTextChannels = interaction.guild.channels.cache.filter(
+      (c) => c.type === ChannelType.GuildText
+    ).size;
+    const guildVoiceChannels = interaction.guild.channels.cache.filter(
+      (c) => c.type === ChannelType.GuildVoice
+    ).size;
+    const guildCategory = interaction.guild.channels.cache.filter(
+      (c) => c.type === ChannelType.GuildCategory
+    ).size;
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: ЭМОДЗИ
+     * ! --------------------------------
+     */
+    const guildEmojiCount = interaction.guild.emojis.cache.size;
+    const guildEmojiAnimate = interaction.guild.emojis.cache.filter(
+      (e) => e.animated
+    ).size;
+    const guildEmojiStatic = interaction.guild.emojis.cache.filter(
+      (e) => !e.animated
+    ).size;
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: EMBED
+     * ! --------------------------------
+     */
     const embedFields = [
       {
         name: "Информация о сервере",
         value:
-          `**- Название :** ${interaction.guild.name}\n` +
-          `**- Создатель :** <@${interaction.guild.ownerId}>\n` +
-          `**- Создан :** <t:${parseInt(
-            interaction.guild.createdTimestamp / 1000
-          )}:R>`,
+          `**- Название :** ${guildName}\n` +
+          `**- Создатель :** <@${guildOwner}>\n` +
+          `**- Создан :** <t:${guildCreatedTimestamp}:R>`,
       },
       {
         name: "📯┃Пользователи",
         value:
-          `**Всего :** ${interaction.guild.memberCount}\n` +
-          `ㅤ*- Люди* : ${
-            interaction.guild.members.cache.filter((m) => !m.user.bot).size
-          }\n` +
-          `ㅤ*- Боты* : ${
-            interaction.guild.members.cache.filter((m) => m.user.bot).size
-          }\n`,
+          `**Всего :** ${guildMemberCount}\n` +
+          `ㅤ*- Люди* : ${guildMembers}\n` +
+          `ㅤ*- Боты* : ${guildBots}\n`,
       },
       {
         name: "📢┃Каналы",
         value:
-          `**Всего :** ${interaction.guild.channels.cache.size}\n` +
-          `ㅤ*- Текстовые* : ${
-            interaction.guild.channels.cache.filter(
-              (c) => c.type === ChannelType.GuildText
-            ).size
-          }\n` +
-          `ㅤ*- Голосовые* : ${
-            interaction.guild.channels.cache.filter(
-              (c) => c.type === ChannelType.GuildVoice
-            ).size
-          }\n` +
-          `ㅤ*- Категории* : ${
-            interaction.guild.channels.cache.filter(
-              (c) => c.type === ChannelType.GuildCategory
-            ).size
-          }\n`,
+          `**Всего :** ${guildChannels}\n` +
+          `ㅤ*- Текстовые* : ${guildTextChannels}\n` +
+          `ㅤ*- Голосовые* : ${guildVoiceChannels}\n` +
+          `ㅤ*- Категории* : ${guildCategory}\n`,
       },
       {
         name: "😁┃Эмодзи",
         value:
-          `**Всего :** ${interaction.guild.emojis.cache.size}\n` +
-          `ㅤ*- Анимированные* : ${
-            interaction.guild.emojis.cache.filter((e) => e.animated).size
-          }\n` +
-          `ㅤ*- Статичные* : ${
-            interaction.guild.emojis.cache.filter((e) => !e.animated).size
-          }\n`,
+          `**Всего :** ${guildEmojiCount}\n` +
+          `ㅤ*- Анимированные* : ${guildEmojiAnimate}\n` +
+          `ㅤ*- Статичные* : ${guildEmojiStatic}\n`,
       },
     ];
     const embedThumbnailImage = {
       url: interaction.guild.iconURL() || "https://i.imgur.com/ZvDmhN9.png",
     };
-    if (interaction.message.author.bot) {
-      embedSetup.color = 0x7289da;
-    }
     await interaction.reply({
       embeds: [
         embedSetup("О сервере", "", embedFields, 0xffffff, embedThumbnailImage),
