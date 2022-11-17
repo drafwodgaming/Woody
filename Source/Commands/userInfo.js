1887c9951c5823baa73bd45b0c19b9e09ef12802
drafwod <drafwodgaming@yandex.ru>
2022-11-18 01:06:53 +0300

    update 0.3

--- Source/Commands/userInfo.js
+++ Source/Commands/userInfo.js
@@ -1,5 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
+const moment = require("moment");
 const { embedSetup } = require("../Functions/embedSetup");
+const botConfig = require("../../Config/botConfig");
+moment.updateLocale(botConfig.momentLocale, {
+  weekdays: botConfig.momentWeekList.split("_"),
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,8 +14,29 @@ module.exports = {
       option.setName("user").setDescription("Пользователи").setRequired(true)
     ),
   async execute(interaction) {
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: ПОЛЬЗОВАТЕЛЯ
+     * ! --------------------------------
+     */
     const targetUser = interaction.options.getUser("user");
     const userCreatedTimestamp = parseInt(targetUser.createdTimestamp / 1000);
+
+    /**
+     * ! --------------------------------
+     * ! ПЕРЕМЕННЫЕ: ПОЛЬЗОВАТЕЛЯ НА СЕРВЕРЕ
+     * ! --------------------------------
+     */
+    const guildMember = interaction.guild.members.cache.get(targetUser.id);
+    const memberJoinedAt = moment(guildMember.joinedAt).format(
+      "dddd, DD.MM.YYYY HH:mm"
+    );
+    const memberRoles = guildMember.roles.cache
+      .map((r) => r)
+      .sort((a, b) => b.position - a.position)
+      .filter((r) => r.id != interaction.guild.id)
+      .join(" ");
+
     /**
      * ! --------------------------------
      * ! ПЕРЕМЕННЫЕ: EMBED
@@ -20,14 +46,16 @@ module.exports = {
       {
         name: "Информация о пользователе",
         value:
-          `**Никнейм :** ${targetUser} \n` +
+          `**Никнейм :** ${targetUser} (${targetUser.tag}) \n` +
           `**ID :** ||${targetUser.id}||\n` +
-          `**Зарегистрирован :** <t:${userCreatedTimestamp}:R>`,
+          `**Зарегистрирован :** <t:${userCreatedTimestamp}:R>\n`,
         inline: true,
       },
       {
         name: "ㅤ",
-        value: `**TAG :** ${targetUser.tag}\n`,
+        value:
+          `**Присоединился к серверу :** \`${memberJoinedAt}\` \n` +
+          `**Роли пользователя :**\n ${memberRoles}`,
         inline: true,
       },
     ];
